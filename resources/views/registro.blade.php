7eaab80317fa340c1b4fe61c0c3ae6065784144c
Miguel Martinez <mike30_mdo_90@hotmail.com>
2019-05-15 13:35:03 -0500

    Front- Formulario de registro, Desktop.

--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -93,18 +93,126 @@
 
     }
 
+    .tituloregistro{
+        text-align: center;
+        font-size: 55px;
+    }
+
+    .recuadro{
+        width: 90%;
+        height: 450px;
+        background: #a90000c2;
+        margin-left: auto;
+        margin-right: auto;
+
+        border-radius: 50px 50px 50px 50px;
+        -moz-border-radius: 50px 50px 50px 50px;
+        -webkit-border-radius: 50px 50px 50px 50px;
+        border: 0px none #000000;
+
+        -webkit-box-shadow: 10px 10px 19px -5px rgba(0,0,0,0.75);
+        -moz-box-shadow: 10px 10px 19px -5px rgba(0,0,0,0.75);
+        box-shadow: 10px 10px 19px -5px rgba(0,0,0,0.75);
+
+        padding-left: 40px;
+        padding-right: 40px;
+        padding-top: 45px;
+
+        text-align: center;
+    }
+
+
     .texto {
         font-family: "texto" !important;
     }
+    .txtregistro{
+        font-family: "texto" !important;
+        color: white;
+        font-style: italic;
+
+        letter-spacing: 0.5pt;
+
+
+    }
+
+
 
 
     .divregistro{
         position: absolute;
         left: 50%;
-        top: 50%;
+        top: 45%;
         transform: translate(-50%, -50%);
         -webkit-transform: translate(-50%, -50%);
+
+        height: 80%;
+        width: 25%;
+
+        /*background: rgba(0,0,0,0.5);*/
+
+        z-index: 1;
     }
+
+    .esquinasr{
+        border-radius: 50px 50px 50px 50px;
+        -moz-border-radius: 50px 50px 50px 50px;
+        -webkit-border-radius: 50px 50px 50px 50px;
+        border: 0px none #000000;
+    }
+
+    .divboton{
+        text-align: center;
+    }
+
+    .botonfonfig{
+        margin-top: 15px;
+        margin-bottom: 15px;
+        background: #a90000c2;
+        border-color: white;
+
+        border-radius: 50px 50px 50px 50px;
+        -moz-border-radius: 50px 50px 50px 50px;
+        -webkit-border-radius: 50px 50px 50px 50px;
+
+        width: 60%;
+    }
+
+    .btn-primary:hover {
+        color: #fff;
+        background-color: #980000;
+        border: 0px none #000000;
+    }
+
+    .btn-primary:not(:disabled):not(.disabled):active, .show>.btn-primary.dropdown-toggle {
+        color: #fff;
+        background-color: #980000;
+        border: 0px none #000000;
+    }
+
+    .btn-primary.focus, .btn-primary:focus {
+        box-shadow: 0 0 0 0.0rem rgba(0,0,0,0);
+    }
+
+    .btn.focus, .btn:focus {
+        outline: 0;
+        box-shadow: 0 0 0 0.0rem rgba(0,0,0,0);
+    }
+
+    *::-moz-selection{
+        background: none;
+    }
+
+    *::selection {
+        background: none;
+    }
+
+    @media (max-width: 767px) {
+        .extremos{
+           display: none;
+        }
+    }
+
+
 
 </style>
 
@@ -112,12 +220,44 @@
 <div id="contenido">
 
     <div class="divregistro">
-        <h1>HOLA</h1>
+        <div>
+            <div>
+                <h1 class="titulo tituloregistro">REGISTRA TU CÓDIGO</h1>
+            </div>
+            <form>
+                <div class="recuadro">
+
+                    <div class="form-group ">
+                        <label for="vin" class="txtregistro">NÚMERO VIN DEL AUTO</label>
+                        <input type="text" class="form-control esquinasr" id="vin">
+                    </div>
+
+                    <div class="form-group ">
+                        <label for="nombre" class="txtregistro">NOMBRE COMPLETO</label>
+                        <input type="text" class="form-control esquinasr" id="nombre">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email" class="txtregistro">CORREO ELECTRÓNICO</label>
+                        <input type="email" class="form-control esquinasr" id="email" aria-describedby="emailHelp">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="telefono" class="txtregistro">TELÉFONO CELULAR</label>
+                        <input type="text" class="form-control esquinasr" id="telefono">
+                    </div>
+
+                </div>
+                <div class="divboton">
+                    <button type="submit" class="btn btn-primary botonfonfig txtregistro">ACEPTAR</button>
+                </div>
+            </form>
+        </div>
     </div>
 
-    <div class=" row">
+    <div class=" row extremos">
 
-        <div class="col-md-7 imizq d-flex align-items-end ">
+        <div class="col-md-7 imizq  d-flex align-items-end ">
             <img src="img/pareja.png" alt="" width="" class="imginterna1">
         </div>
         <div class="col-md-5 imder ">
@@ -133,7 +273,7 @@
         </div>
         <div class="col-md-6 ">
             <div class="">
-                <h4 class="copyfooter texto">
+                <h4 class="copyfooter ">
                     © Toyota México 2019.
                 </h4>
             </div>
